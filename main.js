c8f93ffaefc59cf45f93ce663cc4e22905ec44e9
Cindy Sweetpea <cindychau24@me.com>
2020-01-09 14:59:47 -0500

    blue layout

---
+++ main.js
@@ -0,0 +1,25 @@
+const simpleLogo = document.querySelector('.simplelogo')
+const complexLogo = document.querySelector('.complexlogo')
+const headerArrow = document.querySelector('.fa-angle-down');
+
+const aboutLogos = document.querySelector('.about--wrapper').children
+
+
+TweenMax.staggerFrom(aboutLogos, 1.5, {
+    delay: 1,
+    opacity: 0,
+    y: 60,
+    ease: Expo.easeInOut
+}, 0.3)
+
+// TweenMax.from([simpleLogo, complexLogo], 1.2, {
+//     opacity: 0,
+//     y: -30,
+//     delay: 1.1,
+//     ease: Expo.easeInOut
+// })
+
+TweenMax.to(headerArrow, .4, {
+    repeat: 2,
+    y: 20
+})
