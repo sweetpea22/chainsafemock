4bca1c8d5b7a3ef28f26f883aebe5034f1f008ba
Cindy Sweetpea <cindychau24@me.com>
2020-01-11 19:10:52 -0500

    test scroll animations

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const simpleLogo = document.querySelector('.simplelogo')
 const complexLogo = document.querySelector('.complexlogo')
 const headerArrow = document.querySelector('.fa-angle-down');
-
 const aboutLogos = document.querySelector('.about--wrapper').children
-const landing = document.querySelector('.jumbotron')
 
-TweenMax.staggerFrom(aboutLogos, 1.5, {
-    delay: 1,
-    opacity: 0,
-    y: 60,
-    ease: Expo.easeInOut
-}, 0.3)
+
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        TweenMax.staggerFrom(aboutLogos, 1, {
+            opacity: 0,
+            y: 60,
+            ease: Expo.easeInOut
+        }, 0.3)
+    }
+}
 
 // TweenMax.from([simpleLogo, complexLogo], 1.2, {
 //     opacity: 0,
@@ -24,7 +28,3 @@ TweenMax.to(headerArrow, .4, {
     y: 20
 })
 
-TweenMax.from(landing, .7, {
-    opacity: 0,
-    ease: Expo.easeInOut
-})
